a1f6227df7668b1956b61deaf7ef5a04d75c5570
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-08-08 13:58:28 +0200

    Created a react project setup and added bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 class="p-5">Hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
